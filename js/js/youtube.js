ce67790a1c560e398c64a837df331d1c6db766bf
Wilson <wilson.ck@gmail.com>
2015-03-17 17:01:48 -0300

    Ajustes iniciais nop Javascript

--- js/js/youtube.js
+++ js/js/youtube.js
@@ -1,82 +1,55 @@
-var Youtube = Youtube  || {};
+var App = App  || {};
 
-(function($, namespace) { 
+App.objSearch = (function($) { 
     "use strict";
 
-    namespace = {
-
-        init: function(){
-
-            this.authApiGoogle();
-
-        },
-
-        authApiGoogle: function(){
-
-            // you need to register your own client ID.
-            var OAUTH2_CLIENT_ID = '495419653761-srcto3cnujje92jurnka3sqrnuqqinip.apps.googleusercontent.com';
-            var OAUTH2_SCOPES = [
-              'https://www.googleapis.com/auth/youtube'
-            ];
-
-            // Upon loading, the Google APIs JS client automatically invokes this callback.
-            googleApiClientReady = function() {
-              gapi.auth.init(function() {
-                window.setTimeout(checkAuth, 1);
-              });
-            }
-
-            // Attempt the immediate OAuth 2.0 client flow as soon as the page loads.
-            // If the currently logged-in Google Account has previously authorized
-            // the client specifie  d as the OAUTH2_CLIENT_ID, then the authorization
-            // succeeds with no user intervention. Otherwise, it fails and the
-            // user interface that prompts for authorization needs to display.
-            function checkAuth() {
-              gapi.auth.authorize({
-                client_id: OAUTH2_CLIENT_ID,
-                scope: OAUTH2_SCOPES,
-                immediate: true
-              }, handleAuthResult);
-            }
-
-            // Handle the result of a gapi.auth.authorize() call.
-            function handleAuthResult(authResult) {
-              if (authResult) {
-                // Authorization was successful. Hide authorization prompts and show
-                // content that should be visible after authorization succeeds.
-                $('.pre-auth').hide();
-                $('.post-auth').show();
-                loadAPIClientInterfaces();
-              } else {
-                // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
-                // client flow. The current function is called when that flow completes.
-                $('#login-link').click(function() {
-                  gapi.auth.authorize({
-                    client_id: OAUTH2_CLIENT_ID,
-                    scope: OAUTH2_SCOPES,
-                    immediate: false
-                    }, handleAuthResult);
-                });
-              }
-            }
-
-            // Load the client interfaces for the YouTube Analytics and Data APIs, which
-            // are required to use the Google APIs JS client. More info is available at
-            // http://code.google.com/p/google-api-javascript-client/wiki/GettingStarted#Loading_the_Client
-            function loadAPIClientInterfaces() {
-              gapi.client.load('youtube', 'v3', function() {
-                handleAPILoaded();
-              });
-            }
-
-        }
-
+    function objSearch(){
 
     }
 
+    objSearch.create = function() {
+        return new objSearch();
+    }
 
-    $(function() {
-        namespace.init();
-        console.log('this');
-    });
-})(jQuery, Youtube);
+    objSearch.fn = objSearch.prototype;
+
+    objSearch.fn.init = function(){
+        
+    }
+
+    return objSearch;
+
+})(jQuery);
+
+         
+$(function() {
+    var app = App.objSearch.create();
+    app.init();
+});
+
+
+
+$(function() {
+
+    // After the API loads, call a function to enable the search box.
+    function handleAPILoaded() {
+      $('#search-button').attr('disabled', false);
+    }
+
+    // Search for a specified string.
+    function search() {
+      var q = $('#query').val();
+      var request = gapi.client.youtube.search.list({
+        q: q,
+        part: 'snippet'
+      });
+
+      request.execute(function(response) {
+        var str = JSON.stringify(response.result);
+        $('#search-container').html('<pre>' + str + '</pre>');
+      });
+    }
+
+
+
+  });
